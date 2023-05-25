2dfcceafe6c078dd497d70f5b2d9d4bd691b2b59
Kayla S <kaylaesmith1@gmail.com>
2023-05-25 08:41:31 +0000

    Troubleshooting heroku open app. Make new migrations

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost' 'react-social-media-kes.herokuapp.com']
+ALLOWED_HOSTS = ['localhost:8000' 'react-social-media-kes.herokuapp.com']
 # '8000-kaylaesmith1-drfapi-iy1cqe35k5c.ws-eu97.gitpod.io', 
 
 
